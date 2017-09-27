bd6587e91bcb5fb1071abbccb0294b209471e130
TRI VI <trivi2710@gmail.com>
2017-09-27 00:10:27 -0400

    adding google custom search
    Adding google custom search engine and search for the query based on
    the value of keyword and offset

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
@@ -12,15 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 var search = require('./routes/search.js');
-app.use('/search', search);
+app.use('/api', search);
 
 app.get('/', function (req,res){
 	res.render('mainpage');
 
-});
-
-app.get('/history', function (req,res){
-	res.send('This is history page');
 });
 
 app.listen(process.env.PORT || 3000, function(){
--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const google = require('googleapis');
+const customsearch = google.customsearch('v1');
+const cx = "008928106123010811591:d9gjf59qmjg";
+const apiKey = "AIzaSyCXdQbIQSiHAIA06DMfHQ7Ypr2Pc_SwyQ4";
 
 
+router.get('/search/', function (req,res){
+	var keyword = req.query.keyword;
+	var offset = req.query.offset;
+	customsearch.cse.list({
+		cx: cx,
+		q: keyword,
+		auth: apiKey
+	}, function(err, resp){
+		if(err){
+			return err;
+		}
+		console.log('Result: ' + resp.searchInformation.formattedTotalResults);
+		res.send(resp);
+	})
+	
+	
+	//console.log('keyword: ' + keyword + "; offset: " + offset);
+	//res.send('keyword: ' + keyword + "; offset: " + offset);
+
+	//Example API link call:
+	//https://www.googleapis.com/customsearch/v1?key=AIzaSyCXdQbIQSiHAIA06DMfHQ7Ypr2Pc_SwyQ4&cx=008928106123010811591:d9gjf59qmjg%20Public%20URLGet%20code&q=%22cat%22
+});
 module.exports = router;
