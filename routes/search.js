627681b9e0f39897b04a5541a184615a8c24f017
TRI VI <trivi2710@gmail.com>
2017-09-27 15:35:33 -0400

    Save search query to db and display when user redirects to history link
    Feat:
    Save search query to db and display when user redirects to history link

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const google = require('googleapis');
+var Search = require('../models/search.js');
 
 //Required infos for customer search;
 const customsearch = google.customsearch('v1');
@@ -12,6 +13,19 @@ router.get('/search/', function (req,res){
 	var keyword = req.query.keyword;
 	var offset = req.query.offset;
 	var num = req.query.num;
+	//Save searching query into database:
+	var searchObj = new Search({
+		date: new Date(),
+		keyword: keyword
+	});
+	searchObj.save(function (err){
+		if(err){
+			return err;
+		}
+		console.log("saved");
+	});
+
+	//Custom Search Start
 	customsearch.cse.list({
 		cx: cx,
 		q: keyword,
@@ -24,7 +38,7 @@ router.get('/search/', function (req,res){
 		}
 		//console.log('Result: ' + resp.searchInformation.formattedTotalResults);
 		var imageArray = [];
-		resp.items.forEach(function(item){
+		resp.items.forEach(function (item){
 			//Create an obj with specified property
 			var imageObj ={
 				title: item.title.slice(0, item.title.indexOf("-")-1),
@@ -40,4 +54,14 @@ router.get('/search/', function (req,res){
 		res.send(JSON.stringify(imageArray));
 	});
 });
+
+router.get('/history', function (req,res){
+	Search.find({}, {_id:0, __v:0}, function (err, search){
+		if(err){
+			console.log(err);
+			return;
+		}
+		res.send(search);
+	})
+})
 module.exports = router;
