30f5aaa3bca9181da2129ac977b3215d6d74a34a
TRI VI <trivi2710@gmail.com>
2017-09-27 11:59:26 -0400

    refine result from custom search engine
    Feat:
    1. Refine custom search engine result;
    2. Store result in array
    3. Render it to api/search route

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const google = require('googleapis');
+
+//Required infos for customer search;
 const customsearch = google.customsearch('v1');
 const cx = "008928106123010811591:d9gjf59qmjg";
 const apiKey = "AIzaSyCXdQbIQSiHAIA06DMfHQ7Ypr2Pc_SwyQ4";
@@ -9,23 +11,33 @@ const apiKey = "AIzaSyCXdQbIQSiHAIA06DMfHQ7Ypr2Pc_SwyQ4";
 router.get('/search/', function (req,res){
 	var keyword = req.query.keyword;
 	var offset = req.query.offset;
+	var num = req.query.num;
 	customsearch.cse.list({
 		cx: cx,
 		q: keyword,
-		auth: apiKey
+		auth: apiKey,
+		num: num,
+		start: offset
 	}, function(err, resp){
 		if(err){
 			return err;
 		}
-		console.log('Result: ' + resp.searchInformation.formattedTotalResults);
-		res.send(resp);
-	})
-	
-	
-	//console.log('keyword: ' + keyword + "; offset: " + offset);
-	//res.send('keyword: ' + keyword + "; offset: " + offset);
+		//console.log('Result: ' + resp.searchInformation.formattedTotalResults);
+		var imageArray = [];
+		resp.items.forEach(function(item){
+			//Create an obj with specified property
+			var imageObj ={
+				title: item.title.slice(0, item.title.indexOf("-")-1),
+				link: item.link,
+				snippet: item.snippet,
+				id: item.cacheId
+			};
 
-	//Example API link call:
-	//https://www.googleapis.com/customsearch/v1?key=AIzaSyCXdQbIQSiHAIA06DMfHQ7Ypr2Pc_SwyQ4&cx=008928106123010811591:d9gjf59qmjg%20Public%20URLGet%20code&q=%22cat%22
+			//push obj into array
+			imageArray.push(imageObj);
+			
+		});
+		res.send(JSON.stringify(imageArray));
+	});
 });
 module.exports = router;
