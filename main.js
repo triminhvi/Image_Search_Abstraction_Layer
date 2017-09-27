80e5f0bc80abc2b5dc348838ee3b9654354fa359
TRI VI <trivi2710@gmail.com>
2017-09-27 15:34:17 -0400

    Adding database config and check connection to server
    Feat:
    Adding database config and check connection to server

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/imagesearch');
+var db = mongoose.connection;
+
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
@@ -9,11 +14,26 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 var search = require('./routes/search.js');
 app.use('/api', search);
 
+//DB:
+db.once('error', function(err){
+	if (err){
+		throw err;
+	}
+});
+db.on('open', function(err){
+	if(err){
+		console.log(err);
+	}
+	console.log('Connected to DB');
+})
+var Search = require('./models/search.js');
+
+//APP start
 app.get('/', function (req,res){
 	res.render('mainpage');
 
